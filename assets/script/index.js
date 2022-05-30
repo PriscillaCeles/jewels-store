97289bb9466b3844df6eea59e96e00556509eafa
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-05-30 16:27:50 -0300

    index, CSS and JS update
    Mobile menu construction.

--- assets/script/index.js
+++ assets/script/index.js
@@ -77,3 +77,79 @@ function constructMenuDesktop(json) {
 
 informativesTop();
 // requestMenuDesktop();
+
+//* HOVER MENU 
+const $menuNavDepartment = document.querySelectorAll(".menu__nav-department-list--item")
+const $menuNavCategory = document.querySelectorAll(".menu__nav-wrapper-category")
+
+$menuNavDepartment.forEach(function ($department, index) {
+  $department.addEventListener("mouseover", function (){
+    for (let i = 0; i < $menuNavCategory.length; i++) {
+      const $category = $menuNavCategory[index];
+      $category.classList.add("menu__nav-wrapper-category--active")
+    }
+  })
+  $department.addEventListener("mouseleave", function (){
+    for (let i = 0; i < $menuNavCategory.length; i++) {
+      const $category = $menuNavCategory[index];
+      $category.classList.remove("menu__nav-wrapper-category--active")
+    }
+  })
+})
+
+//* MENU MOBILE */
+
+function handlToggleMenu() {
+  const $openMenuMobile = document.querySelector(".menu__hamburger");
+  const $menuMobile = document.querySelector(".menu__mobile");
+  const $closeMenuMobile = document.querySelector(".close-menu");
+  const $closeMenuMobileIcon = document.querySelector(".menu__mobile-close--button");
+
+  $openMenuMobile.addEventListener("click", function(){
+    $menuMobile.classList.add("active");
+  })
+
+  $closeMenuMobile.addEventListener("click", function(){
+    $menuMobile.classList.remove("active");
+  })
+
+  $closeMenuMobileIcon.addEventListener("click", function(){
+    $menuMobile.classList.remove("active");
+  })
+}
+
+function handlNavMenu() {
+  const $menuMobileArrowDepartments = document.querySelectorAll(".departmentArrow")
+  const $menuMobileCategories = document.querySelectorAll(".menu__mobile-nav-category")
+  const $menuMobileArrowCategories = document.querySelectorAll(".categoryArrow")
+  const $menuMobileSubcategories = document.querySelectorAll(".menu__mobile-nav-subcategory")
+  
+  $menuMobileArrowDepartments.forEach(function ($mobileDepartment, index) {
+    $mobileDepartment.addEventListener("click", function (){
+      for (let i = 0; i < $menuMobileCategories.length; i++) {
+        const $mobileCategory = $menuMobileCategories[index];
+        $mobileCategory.classList.toggle("active-category")
+      }
+  })
+  })
+
+  $menuMobileArrowCategories.forEach(function ($mobileCategory, index) {
+  $mobileCategory.addEventListener("click", function (){
+    for (let i = 0; i < $menuMobileSubcategories.length; i++) {
+      console.log('ENTROU')
+      const $mobileSubcategory = $menuMobileSubcategories[index];
+      $mobileSubcategory.classList.toggle("active-subcategory")
+      console.log($menuMobileSubcategories[index])
+    }
+
+    // for (const $mobileSubcategory of $menuMobileSubcategories) {
+    //   $mobileSubcategory.classList.toggle("active-subcategory");
+    //   console.log(index)
+    // }
+
+  })
+  })
+}
+
+handlToggleMenu()
+handlNavMenu()
